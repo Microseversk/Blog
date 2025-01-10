3989f22abaa2291f4d5c55b7c7e6060f5bd893b7
Microseversk <alexander.osipov.work@gmail.com>
2025-01-11 02:05:19 +0700

    refactoring/issue-9 🦝 Неприличная демонстрация
    1. Неприличная демонстрация
    2.
    a. Скрытие полей класса
    b. Поля могут потенциально быть использованы там, где не должны
    3. Улучшилась устойчивость к багам.

--- Controller/CommunitiesController.js
+++ Controller/CommunitiesController.js
@@ -2,19 +2,19 @@ import { CommunitiesModel } from '../Model/CommunitiesModel.js';
 import { CommunitiesView } from '../View/CommunitiesView.js';
 
 class CommunitiesController {
-  model;
-  view;
+  #model;
+  #view;
 
   constructor() {
-    this.model = new CommunitiesModel();
-    this.view = new CommunitiesView();
+    this.#model = new CommunitiesModel();
+    this.#view = new CommunitiesView();
   }
 
   async init() {
-    const template = await this.model.getCommunityTemplate();
-    const communities = await this.model.getCommunities();
+    const template = await this.#model.getCommunityTemplate();
+    const communities = await this.#model.getCommunities();
 
-    await this.view.renderCommunities(template, communities, this.model.doBtnAction);
+    await this.#view.renderCommunities(template, communities, this.#model.doBtnAction);
   }
 }
 
