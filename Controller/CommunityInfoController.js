7ea7e89ea16ee0cba3d5b9d2fef3ed502dfcb05b
Microseversk <alexander.osipov.work@gmail.com>
2025-01-11 02:14:32 +0700

    refactoring/issue-9 🦝 Неприличная демонстрация
    1. Неприличная демонстрация
    2.
    a. Скрытие полей класса
    b. Поля могут потенциально быть использованы там, где не должны
    3. Улучшилась устойчивость к багам.

--- Controller/CommunityInfoController.js
+++ Controller/CommunityInfoController.js
@@ -4,63 +4,56 @@ import { buildQuery, smoothScrollToTop } from '../Functions/functions.js';
 import { GO_NEXT_PAGE, GO_PREVIOUS_PAGE } from '../Constants/dimens.js';
 
 class CommunityInfoController {
-  model;
-  view;
-  communityId;
-
-  currentQuery = new URLSearchParams(window.location.search);
+  #model;
+  #view;
+  #communityId;
+  #currentQuery;
 
   constructor() {
-    this.model = new CommunityInfoModel();
-    this.view = new CommunityInfoView();
-    this.communityId = window.location.pathname.split('/')[2];
+    this.#model = new CommunityInfoModel();
+    this.#view = new CommunityInfoView();
+    this.#communityId = window.location.pathname.split('/')[2];
+    this.#currentQuery = new URLSearchParams(window.location.search);
   }
 
   async init() {
-    this.view.renderTags(await this.model.getTags());
+    this.#view.renderTags(await this.#model.getTags());
 
-    let info = await this.model.getCommunityInfo(this.communityId);
-    const adminTemplate = await this.model.getAdminTemplate();
+    let info = await this.#model.getCommunityInfo(this.#communityId);
+    const adminTemplate = await this.#model.getAdminTemplate();
 
-    this.view.renderInfo(adminTemplate, info, this.communityId, this.model.doBtnAction);
-    this.view.renderFiltersValues(this.model.getFiltersValuesFromQuery(this.currentQuery));
+    this.#view.renderInfo(adminTemplate, info, this.#communityId, this.#model.doBtnAction);
+    this.#view.renderFiltersValues(this.#model.getFiltersValuesFromQuery(this.#currentQuery));
 
-    const posts = await this.model.getCommunityPosts(
-      this.communityId,
-      this.currentQuery.toString()
-    );
+    const posts = await this.#model.getCommunityPosts(this.#communityId, this.#currentQuery.toString());
 
-    this.view.renderPosts(await this.model.getPostTemplate(), posts);
+    this.#view.renderPosts(await this.#model.getPostTemplate(), posts);
 
-    this.view.renderPagination(this.model.currentPage, this.model.currentPageCount);
+    this.#view.renderPagination(this.#model.currentPage, this.#model.currentPageCount);
 
     let btns = document.querySelectorAll('.number');
     btns.forEach((btn) =>
       btn.addEventListener('click', async () => {
-        this.currentQuery.set('page', btn.innerText);
-        window.location.search = this.currentQuery.toString();
+        this.#currentQuery.set('page', btn.innerText);
+        window.location.search = this.#currentQuery.toString();
       })
     );
   }
 
   async useFilters() {
-    const query = buildQuery(this.view.getFiltersValues());
+    const query = buildQuery(this.#view.getFiltersValues());
     window.location.search = query.toString();
   }
 
   async switchPage(param) {
     if (param === GO_NEXT_PAGE) {
-      this.model.currentPage =
-        this.model.currentPage < this.model.currentPageCount
-          ? this.model.currentPage + 1
-          : this.model.currentPage;
+      this.#model.currentPage =
+        this.#model.currentPage < this.#model.currentPageCount ? this.#model.currentPage + 1 : this.#model.currentPage;
     } else if (param === GO_PREVIOUS_PAGE) {
-      this.model.currentPage =
-        this.model.currentPage > 1 ? this.model.currentPage - 1 : this.model.currentPage;
+      this.#model.currentPage = this.#model.currentPage > 1 ? this.#model.currentPage - 1 : this.#model.currentPage;
     }
 
-    const query = buildQuery(this.view.getFiltersValues(this.model.currentPage));
-    window.location.search = query;
+    window.location.search = buildQuery(this.#view.getFiltersValues(this.#model.currentPage));
   }
 }
 
