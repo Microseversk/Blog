a3793093a7f70fd2632d3a425a413fa8991c9d50
Microseversk <alexander.osipov.work@gmail.com>
2025-01-11 02:18:13 +0700

    refactoring/issue-9 🦝 Неприличная демонстрация
    1. Неприличная демонстрация
    2.
    a. Скрытие полей класса
    b. Поля могут потенциально быть использованы там, где не должны
    3. Улучшилась устойчивость к багам.

--- Controller/CreatePostController.js
+++ Controller/CreatePostController.js
@@ -11,56 +11,57 @@ import {
 } from '../Functions/functions.js';
 
 class CreatePostController {
-  model;
-  view;
-  query;
-
-  currentAddressLevel = 1;
-  addressInfo = [
-    {
-      id: 0,
-      guid: '',
-      text: 'Страна'
-    }
-  ];
+  #model;
+  #view;
+  #currentQuery;
+  #currentAddressLevel;
+  #addressInfo;
 
   constructor() {
-    this.model = new CreatePostModel();
-    this.view = new CreatePostView();
+    this.#model = new CreatePostModel();
+    this.#view = new CreatePostView();
     this.query = new URLSearchParams(window.location.search);
+    this.#currentAddressLevel = 1;
+    this.#addressInfo = [
+      {
+        id: 0,
+        guid: '',
+        text: 'Страна'
+      }
+    ];
   }
 
   async init() {
     await checkToken(getToken(), true);
-    const communities = await this.model.getAdminUsersCommunities();
-    const tags = await this.model.getTags();
-    this.view.renderTags(tags);
-    await this.view.renderCommunities(communities);
-    this.view.renderCommunityValue(this.model.getCommunityValueFromQuery(this.query));
+    const communities = await this.#model.getAdminUsersCommunities();
+    const tags = await this.#model.getTags();
+    this.#view.renderTags(tags);
+    await this.#view.renderCommunities(communities);
+    this.#view.renderCommunityValue(this.#model.getCommunityValueFromQuery(this.#currentQuery));
 
-    await addAddressSelect(0, this.currentAddressLevel, this.addressInfo);
+    await addAddressSelect(0, this.#currentAddressLevel, this.#addressInfo);
   }
 
   async createPost() {
-    this.view.clearErrors();
-    let data = this.view.getCreatePostData();
+    this.#view.clearErrors();
+    let data = this.#view.getCreatePostData();
 
-    if (this.addressInfo.length > 1 && this.addressInfo[1].guid !== '') {
-      let latestAddressIndex = this.addressInfo.length - 1;
-      while (this.addressInfo[latestAddressIndex].guid === '') {
+    if (this.#addressInfo.length > 1 && this.#addressInfo[1].guid !== '') {
+      let latestAddressIndex = this.#addressInfo.length - 1;
+      while (this.#addressInfo[latestAddressIndex].guid === '') {
         latestAddressIndex -= 1;
       }
-      data.addressId = this.addressInfo[latestAddressIndex].guid;
+      data.addressId = this.#addressInfo[latestAddressIndex].guid;
     }
 
-    const validateResult = this.model.validateCreatePostData(data);
+    const validateResult = this.#model.validateCreatePostData(data);
 
     if (validateResultHasErrors(validateResult)) {
-      this.view.showErrors(validateResult);
+      this.#view.showErrors(validateResult);
       return;
     }
 
-    await this.model.sendNewPostData(data);
+    await this.#model.sendNewPostData(data);
     window.location.href = MAIN_PAGE;
   }
 }
